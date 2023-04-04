a7c9b03079ad4b80e68396ec4c8f599cf5c28d92
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 19:33:29 +0530

    PhoneNumber

--- Junit.java
+++ Junit.java
@@ -6,13 +6,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Junit {
- // Uc3 = As a User need to enter a valid email
-    //E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) wit precise @ and . positions
-    public boolean validEmail(String email){
-        Pattern pattern = Pattern.compile("^[a-z][a-z0-9-.+]*@[a-z0-9]*[.][a-z.]*$");
-        Matcher matcher = pattern.matcher(email);
-        return matcher.find();
-    }
+// Uc4 = As a User need to follow pre -define Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number
+public boolean phoneNumber(String phoneNumber) {
+    Pattern pattern = Pattern.compile("^[+]\\d{2} \\d{10}$");
+    Matcher matcher = pattern.matcher(phoneNumber);
+    return matcher.find();
+}
 
   
 }
