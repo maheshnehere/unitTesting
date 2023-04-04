8a0cc5f2c4cfca76c07ea9ba2d429ea44474e2ce
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 19:31:56 +0530

    Email

--- Junit.java
+++ Junit.java
@@ -6,11 +6,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Junit {
- // Uc2 = As a User need to enter a valid Last Name = Last name starts with Cap and has minimum 3 characters
- public boolean  lastName(String lastName) {
-    Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
-    Matcher matcher = pattern.matcher(lastName);
-    return matcher.find();
-}
-     
+ // Uc3 = As a User need to enter a valid email
+    //E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) wit precise @ and . positions
+    public boolean validEmail(String email){
+        Pattern pattern = Pattern.compile("^[a-z][a-z0-9-.+]*@[a-z0-9]*[.][a-z.]*$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.find();
+    }
+
+  
 }
