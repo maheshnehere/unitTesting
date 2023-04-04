cc257b4b24838f972ef29f2c1f39a943f805cffd
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 19:46:10 +0530

    Create JunitTest.java

---
+++ JunitTest.java
@@ -0,0 +1,13 @@
+package com.JunitTest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JunitTest {
+    Junit runner = new Junit();
+
+   
+}
