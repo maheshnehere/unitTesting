e96d44654da7f4be2e51fb5921c8caecf83b2c69
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 19:36:37 +0530

    5,6,7,8 password sample

--- Junit.java
+++ Junit.java
@@ -6,12 +6,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Junit {
-// Uc4 = As a User need to follow pre -define Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number
-public boolean phoneNumber(String phoneNumber) {
-    Pattern pattern = Pattern.compile("^[+]\\d{2} \\d{10}$");
-    Matcher matcher = pattern.matcher(phoneNumber);
-    return matcher.find();
-}
+    public boolean password(String password){
+        Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[\\d])(?=.*[\\W])[\\w\\W]{8,}");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+
+    }
 
   
 }
